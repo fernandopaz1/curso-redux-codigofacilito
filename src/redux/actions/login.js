d704ca9a8367ec6c6dcbd8f540dc189b05c05260
feranandopaz1 <ferpaz9292@gmail.com>
2021-08-19 13:22:49 -0300

    add: action checkLogin

---
+++ src/redux/actions/login.js
@@ -0,0 +1,13 @@
+import { createAction } from "@reduxjs/toolkit";
+import {
+    CHECK_LOGIN
+} from '../../constants/index'
+
+function checkLogin() {
+    return {
+        type: CHECK_LOGIN,
+        payload: "",
+    }
+}
+
+export default checkLogin
